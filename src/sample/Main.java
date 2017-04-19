b01a7dcc5927435864a02309097ec6649757deca
swesfsa <it15001@lehre.dhbw-stuttgart.de>
2017-04-20 00:13:08 +0200

    Fenster mit Tabs im SceneBuilder erstellt

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,11 +11,9 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello GitHub");
-        Scene s = new Scene(root, 300, 275);
-        primaryStage.setScene(s);
+        primaryStage.setTitle("SFSA - Software für standardisierte Anforderungssammlung");
+        primaryStage.setScene(new Scene(root, 900, 500));
         primaryStage.show();
-        System.out.println("Hallo Tim");
     }
 
 
