c22b41bb99f0336418b2bb9d0011237d41c99013
Thomas Monninger <t.monninger@t-online.de>
2017-04-21 09:14:14 +0200

    Moved Scene instantiation from inline to single line

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,8 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello GitHub");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Scene s = new Scene(root, 300, 275);
+        primaryStage.setScene(s);
         primaryStage.show();
     }
 
