40df3cf60f394fa045eac6b767fd720ef00c3254
swesfsa <marcostierle@gmail.com>
2017-04-23 23:25:32 +0200

    UI v0.1

--- src/sfsa/SFSAController.java
+++ src/sfsa/SFSAController.java
@@ -1,4 +1,37 @@
 package sfsa;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class SFSAController {
+
+    @FXML
+    private TextField zielbestimmungTextField;
+
+    @FXML
+    private Button saveButton;
+
+    @FXML
+    private Button editButton;
+
+    @FXML
+    private Button deleteButton;
+
+    @FXML
+    void saveButtonPressed(ActionEvent event) {
+        zielbestimmungTextField.setEditable(false);
+    }
+
+    @FXML
+    void editButtonPressed(ActionEvent event) {
+        zielbestimmungTextField.setEditable(true);
+    }
+
+    @FXML
+    void deleteButtonPressed(ActionEvent event) {
+        zielbestimmungTextField.deleteText(0, zielbestimmungTextField.getLength());
+    }
+
 }
