3d84a38d7a012e4e51a122fa3e21eec43b5f136d
swesfsa <marcostierle@gmail.com>
2017-04-27 20:38:50 +0200

    UI v0.1

--- src/sfsa/Main.java
+++ src/sfsa/Main.java
@@ -5,16 +5,21 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import view.DeleteWindowView;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("SFSA.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../fxml/SFSA.fxml"));
         primaryStage.setTitle("SFSA - Software für standardisierte Anforderungssammlung");
         Scene scene = new Scene(root, 900, 500);
         primaryStage.setScene(scene);
         primaryStage.show();
+
+        DeleteWindowView deleteWindow = new DeleteWindowView();
+        Stage deleteStage = new Stage();
+        deleteWindow.openDeleteWindow(deleteStage);
     }
 
 
