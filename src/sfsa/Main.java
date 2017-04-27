ce935757dbab59a4b9ab6c7fd32defe53c8be966
swesfsa <marcostierle@gmail.com>
2017-04-27 18:47:38 +0200

    UI v0.1

--- src/sfsa/Main.java
+++ src/sfsa/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("SFSAView.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("SFSA.fxml"));
         primaryStage.setTitle("SFSA - Software für standardisierte Anforderungssammlung");
         Scene scene = new Scene(root, 900, 500);
         primaryStage.setScene(scene);
