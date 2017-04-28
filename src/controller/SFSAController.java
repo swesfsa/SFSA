c5c67831416af7d18b7969b859c3c346b66936f2
swesfsa <marcostierle@gmail.com>
2017-04-29 00:47:43 +0200

    UI v0.1

--- src/controller/SFSAController.java
+++ src/controller/SFSAController.java
@@ -1,25 +1,57 @@
 package controller;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.*;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
-import javafx.scene.text.TextBuilder;
-import javafx.scene.text.TextFlow;
-import view.TargetSpecificationView;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.stage.Stage;
+import view.SFSAView;
 
 public class SFSAController {
 
-    TargetSpecificationView tsview;
+   private SFSAView view;
 
-    @FXML
-    Tab targetSpecificationTab;
+   public SFSAController() throws Exception{
 
-    @FXML
-    public void tabPressed(ActionEvent event) throws Exception {
-        tsview = new TargetSpecificationView();
-    }
+       view = new SFSAView();
+       view.getTargetSpecification().setOnSelectionChanged(new TargetSpecificationTabEventHandler());
+       view.getProductUse().setOnSelectionChanged(new ProductUseTabEventHandler());
+       view.getFunctionalRequirements().setOnSelectionChanged(new FunctionalRequirementsTabEventHandler());
+       view.getProductData().setOnSelectionChanged(new ProductDataTabEventHandler());
+   }
+
+   public void show(Stage stage) {
+       view.show(stage);
+   }
+
+   class TargetSpecificationTabEventHandler implements  EventHandler<Event> {
+
+       @Override
+       public void handle(Event event) {
+           System.out.println("targetSpecificationTab clicked");
+       }
+   }
+
+   class ProductUseTabEventHandler implements  EventHandler<Event> {
+
+       @Override
+       public void handle(Event event) {
+           System.out.println("productUseTab clicked");
+       }
+   }
+
+   class FunctionalRequirementsTabEventHandler implements EventHandler<Event> {
+
+       @Override
+       public void handle(Event event) {
+           System.out.println("functionaleRequirementsTab clicked");
+       }
+   }
+
+   class ProductDataTabEventHandler implements  EventHandler<Event> {
+
+       @Override
+       public void handle(Event event) {
+           System.out.println("productDataTab clicked");
+       }
+   }
+
 }
