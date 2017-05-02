1c069b5cb54cddf9f77d5c4a60cd862562416cf1
swesfsa <marcostierle@gmail.com>
2017-05-03 00:29:17 +0200

    ViewInterfaces added

--- src/controller/FunctionalRequirementsController.java
+++ src/controller/FunctionalRequirementsController.java
@@ -1,7 +1,18 @@
 package controller;
 
+import model.IModel;
+
 /**
  * Created by 1030129 on 02.05.17.
  */
-public class FunctionalRequirementsController {
+public class FunctionalRequirementsController implements IController {
+
+    public FunctionalRequirementsController(IModel model) {
+
+    }
+
+    @Override
+    public void show() {
+
+    }
 }
--- src/sfsa/Main.java
+++ src/sfsa/Main.java
@@ -1,20 +1,19 @@
 package sfsa;
 
-import controller.CreateProductDataController;
-import controller.DeleteWindowController;
-import controller.SFSAController;
+import controller.*;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import misc.FunctionalRequirement;
+import misc.Priority;
+import misc.StageHandler;
 import model.IModel;
+import model.Model;
 import view.CreateProductDataView;
 import view.DeleteWindowView;
 import view.CreateFunctionalRequirementView;
-
-import java.util.List;
 
 public class Main extends Application {
 
@@ -24,7 +23,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        /*  Parent root = FXMLLoader.load(getClass().getResource("../fxml/SFSA.fxml"));
+      /*  Parent root = FXMLLoader.load(getClass().getResource("../fxml/SFSA.fxml"));
         primaryStage.setTitle("SFSA - Software für standardisierte Anforderungssammlung");
         Scene scene = new Scene(root, 900, 500);
         primaryStage.setScene(scene);
@@ -45,8 +44,25 @@ public class Main extends Application {
         //sfsaController.show(primaryStage);
 
         //CreateProductDataController createProductDataController = new CreateProductDataController();
-        //createProductDataController.show();
+        //createProductDataController.show(primaryStage);
 
+/**
+ * @author 9459758
+ * example using mvc model
+ * todo: adding fxml import to view classes
+ */
+        // store Stage instance in Singleton Pattern to be accessed by views
+        StageHandler.getInstance().setPrimaryStage(primaryStage);
+
+        IModel model = new Model();
+        model.addFunctionalRequirement(new FunctionalRequirement(1, "Peter Bohl", "Test", Priority.LOW));
+        model.addFunctionalRequirement(new FunctionalRequirement(2, "Tim Seitz", "ABC", Priority.HIGH));
+        model.addFunctionalRequirement(new FunctionalRequirement(3, "Marco Stierle", "Test", Priority.HIGH));
+        model.addFunctionalRequirement(new FunctionalRequirement(4, "Dr. Dre", "2001", Priority.MIDDLE));
+        model.addFunctionalRequirement(new FunctionalRequirement(5, "Future", "Mask Off", Priority.HIGH));
+
+        IController controller = new FunctionalRequirementsController(model);
+        controller.show();
     }
 
 }
