0c4c8cb21e03f17f7171cfbedfafc202e9c22085
Thomas Monninger <t.monninger@t-online.de>
2017-06-24 13:42:53 -0700

    Added files for FP analysis

--- src/misc/Complexity.java
+++ src/misc/Complexity.java
@@ -3,5 +3,8 @@ package misc;
 /**
  * Created by tmonn on 17.05.2017.
  */
-public class complexity {
+public enum Complexity {
+    LOW,
+    MIDDLE,
+    HIGH;
 }
--- src/misc/Factors.java
+++ src/misc/Factors.java
@@ -4,4 +4,33 @@ package misc;
  * Created by tmonn on 17.05.2017.
  */
 public class Factors {
+    private int integration;
+    private int decentralData;
+    private int transactionRate;
+    private int calculation;
+    private int control;
+    private int exception;
+    private int logic;
+    private int reusability;
+    private int conversion;
+    private int customization;
+
+    public Factors(int integration, int decentralData, int transactionRate, int calculation, int control,
+            int exception, int logic, int reusability, int conversion, int customization) {
+        this.integration = integration;
+        this.decentralData = decentralData;
+        this.transactionRate = transactionRate;
+        this.calculation = calculation;
+        this.control = control;
+        this.exception = exception;
+        this.logic = logic;
+        this.reusability = reusability;
+        this.conversion = conversion;
+        this.customization = customization;
+    }
+
+    public int getFactorSum() {
+        return integration + decentralData + transactionRate + calculation + control +
+                exception + logic + reusability + conversion + customization;
+    }
 }
